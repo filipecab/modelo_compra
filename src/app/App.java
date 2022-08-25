95558d24d326b6657a69a3a6575d0b09ccee48e4
Filipe Matheus <silva.filipe.filipe@gmail.com>
2022-08-25 14:27:28 -0300

    add pastas

---
+++ src/app/App.java
@@ -0,0 +1,14 @@
+package app;
+import java.util.Locale;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc=new Scanner(System.in);
+        
+
+
+
+        
+    }
+}
