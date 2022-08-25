c9713030ca0e43559e51aca01ff805be1b01aa6b
Filipe Matheus <silva.filipe.filipe@gmail.com>
2022-08-25 16:40:07 -0300

    alterações na main

--- src/app/App.java
+++ src/app/App.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 import java.time.format.DateTimeFormatter;
 import cliente.Cliente;
 import ordemDeServico.ordem;
+import status.Status;
+import item.Item;
+import produto.Produto;
 import java.text.ParseException;
 
 
@@ -31,6 +34,57 @@ public class App {
             Cliente cl=new Cliente(nome,email,LocalDate.of(ano,mes,dia));
             System.out.println();
             System.out.println("Enter ordem data: ");
+            System.out.print("Status: ");
+            System.out.print("1 para Aguardando pagamento, 2 para Processando, 3 para Enviado, 4 para Entregue:");
+            int opc=sc.nextInt();
+            String status="";
+            switch (opc){
+                case 1:
+                    status="AGUARDANDO_PAGAMENTO";
+                    break;
+                case 2:
+                    status="PROCESSANDO";
+                    break;
+                case 3:
+                    status="ENVIADO";
+                    break;
+                case 4:
+                    status="ENTREGUE";
+                    break;
+                default:
+                    System.out.println("Erro ao executar!");
+                    status="";
+                
+
+            }
+        
+
+            System.out.print("how many items to this order?");
+            int n=sc.nextInt();
+            System.out.println();
+            for (int i=0;i<n;i++){
+                System.out.println("Enter "+i+1+"item data:");
+                System.out.print("Product name: ");
+                sc.nextLine();
+                String P_name=sc.nextLine();
+                System.out.println();
+                System.out.print("Product price: ");
+                double preco_p=sc.nextDouble();
+                System.out.println();
+                System.out.print("Quantity: ");
+                int q=sc.nextInt();
+                System.out.println();
+                LocalDate momento=LocalDate.now();
+                ordem compra=new ordem(momento,Status.valueOf(status));
+                Produto p=new Produto(P_name,preco_p);
+                Item item=new Item(q,p.getPrecoP(),p);
+            }
+            
+
+
+
+
+
 
 
         }
